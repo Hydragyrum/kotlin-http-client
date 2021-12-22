146fb2dfd1327fe8beab2ff7dc330ce6972c48d0
Hydragyrum <hydragyrum@gmail.com>
2021-12-22 15:23:14 +0100

    Fix issue with headers containing :

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -37,6 +37,12 @@ fun main(args: Array<String>) {
     ).default(false)
     val body by parser.option(ArgType.String, fullName = "body", shortName = "B", description = "Request Body")
     val output by parser.option(ArgType.String, fullName = "output", shortName = "o", description = "Output file")
+    val setExec by parser.option(
+        ArgType.Boolean,
+        fullName = "setExec",
+        shortName = "x",
+        description = "Set the downloaded file as executable"
+    ).default(false)
 
     parser.parse(args)
     val client = HttpClient(CIO)
@@ -46,16 +52,14 @@ fun main(args: Array<String>) {
             this.method = HttpMethod.parse(method)
             this.headers {
                 headers.map {
-                    it.split(":")
-                }.map {
-                    it[0] to it[1]
+                    it.substringBefore(":") to it.substringAfter(":")
                 }.forEach {
                     append(it.first, it.second)
                 }
             }
             body?.let { this.body = it }
 
-            if( verbose ) {
+            if (verbose) {
                 onDownload { bytesSentTotal, contentLength ->
                     println("Received $bytesSentTotal bytes from $contentLength")
                 }
@@ -63,23 +67,24 @@ fun main(args: Array<String>) {
         }
 
         println(response.status)
-        if(verbose) {
+        if (verbose) {
             println(response.headers.flattenEntries().joinToString("\n") { "${it.first}: ${it.second}" })
             println("-".repeat(20))
         }
         val byteArrayBody: ByteArray = response.receive()
-        if( verbose || output.isNullOrBlank()) {
+        if (verbose || output.isNullOrBlank()) {
             println(byteArrayBody.decodeToString())
         }
         if (!output.isNullOrBlank()) {
             val file = File(output)
-            if(!file.exists()) {
-                if(file.parentFile?.toPath()?.exists() != true){
+            if (!file.exists()) {
+                if (file.parentFile?.toPath()?.exists() != true) {
                     file.parentFile?.mkdirs()
                 }
                 file.createNewFile()
             }
             file.writeBytes(byteArrayBody)
+            file.setExecutable(setExec)
             println("Saved to $output")
         }
     }
